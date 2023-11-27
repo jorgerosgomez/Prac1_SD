7eb08f2599f51a01728bb24abb881df2fc6fe81d
jorgerosgomezç <jrg94@alu.ua.es>
2023-11-27 18:57:13 +0100

    actualizacion

--- mapas_js.js
+++ mapas_js.js
@@ -4,6 +4,37 @@ var DroneMapApp = (function() {
     var Mapa = document.querySelector('.Mapa-drones');
     var botonocultar = document.getElementById('ocultarMapa');
     var mapSize = 20; // Tamaño del mapa
+    
+
+    function fetchDronePositions() {
+        fetch('http://127.0.0.1:5000/drones')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(drones => {
+                // Limpiar el mapa antes de actualizar las posiciones
+                clearDronePositions();
+                
+                // Recorre cada dron y actualiza su posición
+                drones.forEach(drone => {
+                    console.log(drone)
+                    updateDronePosition(drone.identificador, drone.posicion[0], drone.posicion[1]);
+                });
+            })
+            .catch(error => {
+                console.error('There was an error fetching the drone positions:', error);
+            });
+    }
+    function clearDronePositions() {
+        const allCells = document.querySelectorAll('.drone-map-cell');
+        allCells.forEach(cell => {
+            cell.textContent = ''; // Eliminar el identificador del dron
+            cell.classList.remove('drone-present'); // Eliminar la clase que indica la presencia de un dron
+        });
+    }
 
     // Función para crear el mapa de drones
     function CrearMapa() {
@@ -27,10 +58,21 @@ var DroneMapApp = (function() {
 
     // Función para actualizar la posición de un dron en el mapa
     function updateDronePosition(droneId, x, y) {
+        // Primero, elimina la clase 'drone-present' de todas las celdas para limpiar estados anteriores
+        document.querySelectorAll('.drone-map-cell.drone-present').forEach(cell => {
+            cell.classList.remove('drone-present');
+        });
+    
+        // Encuentra la celda correspondiente a las coordenadas (x, y)
         const cellSelector = `.drone-map-cell[data-row="${y}"][data-col="${x}"]`;
         const cell = document.querySelector(cellSelector);
-        cell.textContent = droneId; // Ejemplo simple: poner el ID del dron en la celda
-        cell.classList.add('drone-present'); // Suponiendo que tienes estilos para esta clase
+        
+        // Asegúrate de que la celda existe para evitar errores
+        if(cell) {
+            // Actualiza el contenido de la celda con el ID del dron y cambia el color de fondo a verde
+            cell.textContent = droneId;
+            cell.classList.add('drone-present');
+        }
     }
 
     // Inicializa el mapa y los botones
@@ -41,6 +83,8 @@ var DroneMapApp = (function() {
         botonocultar.addEventListener('click', MapaEsVisible);
         // Crear el mapa de drones
         CrearMapa();
+        setInterval(fetchDronePositions,1000);
+    
     }
 
     // Revelar las funciones públicas
